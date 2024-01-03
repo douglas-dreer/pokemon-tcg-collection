a22845dbcb499575ef162b415a382a45de9c716d
Douglas Dreer <douglasdreer@outlook.com.br>
2024-01-03 13:25:10 -0300

    Commit intial

---
+++ src/test/java/com/nintendo/tcg/pokemon/sdk/PokemonTcgSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nintendo.tcg.pokemon.sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTcgSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
